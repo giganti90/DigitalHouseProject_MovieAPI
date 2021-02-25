811ff5c2d1ee66082af40d22cee0c1377384c6e5
ThribsClube <thiago@clubedochaveiro.com.br>
2021-02-25 17:59:04 -0300

    Worked on the login screen layout

---
+++ popfilm/app/src/main/java/com/dhgrupo5/popfilm/activity/Login.kt
@@ -0,0 +1,12 @@
+package com.dhgrupo5.popfilm.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.dhgrupo5.popfilm.R
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
