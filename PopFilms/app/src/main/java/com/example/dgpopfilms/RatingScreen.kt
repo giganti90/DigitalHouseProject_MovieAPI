0d4b20e5c6eb5da10c0afee9ae98b29758e11420
Giga <gabrielgiganti@hotmail.com>
2021-03-13 20:05:44 -0300

    esqueci da tela de avaliação no ultimo commit

---
+++ PopFilms/app/src/main/java/com/example/dgpopfilms/RatingScreen.kt
@@ -0,0 +1,11 @@
+package com.example.dgpopfilms
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RatingScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rating_screen)
+    }
+}
