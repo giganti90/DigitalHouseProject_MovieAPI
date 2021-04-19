09223f5a30af524d52d909970ddd0ac57143ac9a
Giga <gabrielgiganti@hotmail.com>
2021-04-19 17:42:24 -0300

    ultimas tentativas, problemas na activity e adapter do catalog de filmes dentro de uma categoria

--- popfilm/app/src/main/java/com/dhgrupo5/popfilm/pack/model/MovieConfig.kt
+++
@@ -1,8 +0,0 @@
-package com.dhgrupo5.popfilm.pack.model
-
-object MovieConfig {
-    var config: MovieDetail? = null
-    fun setConfiguration(configuration: MovieDetail){
-        config = configuration
-    }
-}
