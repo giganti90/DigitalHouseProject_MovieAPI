f8c98db054af4a32d9687a0ee447f7bf91636698
ThribsClube <thiago@clubedochaveiro.com.br>
2021-05-27 11:55:03 -0300

    firestore

--- popfilm/app/src/main/java/com/dhgrupo5/popfilm/pack/repository/FirebaseRepository.kt
+++ popfilm/app/src/main/java/com/dhgrupo5/popfilm/pack/repository/FirebaseRepository.kt
@@ -1,4 +1,8 @@
 package com.dhgrupo5.popfilm.pack.repository
 
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
 class FirebaseRepository {
+    val firestore = Firebase.firestore
 }
